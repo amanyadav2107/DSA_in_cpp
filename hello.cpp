a53d40c0f9392f0010a6bebe51d794691348f990
Aman Yadav <amanyadavchak@gmail.com>
2026-01-27 17:15:15 +0530

    Updated Base case

--- hello.cpp
+++ hello.cpp
@@ -19,13 +19,13 @@
 using namespace std;
 
 int sum(int n){
-    if(n==0){
+    if(n==1){
         return 1;
     }
     return n+sum(n-1);
 }
 
 int main() {
-    cout<<sum(3);
+    cout<<sum(10);
     return 0;
 }
