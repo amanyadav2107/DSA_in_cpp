b85a47991c5c14724ce47d104ac60779a59c7e46
Aman Yadav <amanyadavchak@gmail.com>
2026-09-13 23:54:48 +0530

    Ignore

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,20 +1,43 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int arr[]= {5,7,7,8,8,10};
+//     bool isFound = false;
+//     int target = 8 ;
+//     int size = sizeof(arr)/sizeof(arr[0]);
+//     for(int i =0; i<size; i++){
+//         if(arr[i]==target){
+//             cout<<"Element found at position : "<<i+1;
+//             isFound=true;
+//             break;
+//         }
+//     }
+//     if(!isFound){
+//         cout<<"Element not found";
+//     }
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int arr[]= {1,2,3,23,55,99,4};
+    int arr[]= {5,7,7,8,8,10};
     bool isFound = false;
-    int target = 3;
+    int target = 8;
     int size = sizeof(arr)/sizeof(arr[0]);
-    for(int i =0; i<size; i++){
-        if(arr[i]==target){
-            cout<<"Element found at position : "<<i+1;
-            isFound=true;
-            break;
+
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == target) {
+            cout << "Element found at position : " << i << endl;
+            isFound = true;
         }
     }
-    if(!isFound){
-        cout<<"Element not found";
+
+    if(!isFound) {
+        cout << "Element not found";
     }
 
     return 0;
