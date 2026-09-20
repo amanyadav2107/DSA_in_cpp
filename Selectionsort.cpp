08fb8bc141d2a5c9e3c7e0a9fad92ae83dd3f6d7
Aman Yadav <amanyadavchak@gmail.com>
2026-09-20 21:24:01 +0530

    Selection Sort

---
+++ Selectionsort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter no. of Elements";
+    cin>>n;
+    int arr[n];
+    for (int i =0; i<n; i++)
+    cin>>arr[i];
+    for (int i=0; i<n-1; i++){
+        int minindex=i;
+        for (int j=i+1; j<n; j++){
+            if( arr[j]<arr[minindex])
+            minindex =j;
+        }
+        if (minindex !=1){
+            int temp=arr[i];
+            arr[i]=arr[minindex];
+            arr[minindex]=temp;
+
+        }
+    }
+    for(int i=0; i<n; i++)
+    cout<<arr[i]<<"  ";
+    return 0;
+}
