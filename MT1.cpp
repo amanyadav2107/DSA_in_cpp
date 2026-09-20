61fde46ee50a022e2dd2bf5a3588600b11a4c81d
Aman Yadav <amanyadavchak@gmail.com>
2026-09-20 21:22:01 +0530

    MT 1

---
+++ MT1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[]={2,4,5,6,7,8};
+    bool isFound= false;
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0; i<size; i++){
+        
+    }
+    return 0;
+}
