2d492788c380a6064f77089118ff2cda4e1cd1e9
Aman Yadav <amanyadavchak@gmail.com>
2026-09-21 21:07:57 +0530

    unique number

---
+++ Uniquenumber.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    set<int>s;
+    int x;
+
+    for(int i=0; i<n; i++){
+        cin>>x;
+        s.insert(x);
+
+    }
+    for(auto num: s){
+        cout<<num<<" ";
+
+    }
+    cout<<endl;
+    cout<<s.size();
+    
+    return 0;
+}
